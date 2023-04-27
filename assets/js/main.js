deacd7aa8532d40f429a43239da474f20d104e77
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-04-27 22:59:43 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,11 @@ let bombs = [];
 let points = 0;
 
 //===startFunzioni==================================================================
-
+//Genera Numeri casuali
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
+//Crea Elementi
 function createElements(tagHtml, classe, contenuto) {
 
     //Creiamo una cella
@@ -20,31 +20,40 @@ function createElements(tagHtml, classe, contenuto) {
     cell.innerText = contenuto;
     return cell;
 }
-
-function clicked(box) {
-
-    let numberSquere = Number(box.innerText);
-    
-
-    if (bombs.includes(numberSquere)) {
-        alert("Hai perso totalizzando " + points + " punti");
-        container.innerHTML = "";
-    }else {
-        points ++;
-        console.log(points);
-    }
-    box.classList.toggle("clicked");
-}
-
+//Crea delle bombe
 function generateBombs(min, max) {
     
-    while(bombs.length !== 16) {
+    while(bombs.length !== 48) {
         const bomb = getRandomNumber(min, max)
         if (!bombs.includes(bomb)) {
             bombs.push(bomb)
         }
     }
 }
+
+function clicked(box) {
+    
+    let numberSquere = Number(box.innerText);
+    
+    if (bombs.includes(numberSquere)) {
+        alert("Hai perso totalizzando " + points + " punti");
+        container.innerHTML = "";
+        points = 0;
+    }else {
+        points ++;
+        console.log(points);
+    }
+    box.classList.replace("unclicked","clicked");
+    
+    if (points == difficulty - 48) {
+        let test = document.querySelectorAll(".unclicked");
+        test.forEach(box => {
+            box.style.backgroundColor = 'red';
+            
+        })
+    }
+}
+
 //===endFunzioni==================================================================
 
 //===startEvent==================================================================
@@ -59,6 +68,7 @@ btnCreateTable.addEventListener("click", function(){
     for (let i = 1; i <= difficulty ; i++) {
         
         let square = createElements("div", "square", i);
+        square.classList.add("unclicked");
         
         if (difficulty == 49) {
             square.classList.add("easySquere");
@@ -76,9 +86,11 @@ btnCreateTable.addEventListener("click", function(){
         
         square.addEventListener("click", function(){
             clicked(square);
-            console.log(i);
+            // console.log(i);
         })
+        
     }
 })
+
 
 
