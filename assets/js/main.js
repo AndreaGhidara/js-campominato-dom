5ebb6e5d7d428f026a7b0d59360c1b91f3744eea
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-05-01 15:28:31 +0200

    Restyling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,108 +1,108 @@
-const container = document.querySelector(".container");
-const btnCreateTable = document.querySelector("#createTable");
-const levelDifficuly = document.querySelector("#difficultyLevel");
+//===FunctionStart======================================================
 
-let difficulty;
+//Funzione per generare un numero casuale in un range.
+function createRandomNumber(min, max) {
+    return Math.floor(Math.random() * (min, max)+min)
+}
+
+//funzione per creare delle "bombe"
+function generateBombs(numberBomb, maxCell) {
+    let bombs = [];
+    
+    while (bombs.length < numberBomb) { //finche l' array non e maggiore del valore dato per creare le bombe
+        let nuovoNumero = createRandomNumber(1, maxCell);
+
+        if( ! bombs.includes(nuovoNumero)) { //se nel array bombs non vi è un numnero uguale a quello appena creato
+            bombs.push(nuovoNumero); //allora lo pusho nel array.
+        }
+    }
+    return bombs;
+}
+
+//Crea un elemento HTML con classe e contenuto
+function createObj(tagHtml, classe, content) {
+    let obj = document.createElement(tagHtml);
+    obj.classList.add(classe);
+    obj.innerText = content;
+
+    return obj;
+}
+
+//===FunctionEnd======================================================
+
+//===ElementStart======================================================
+const btnStartGame = document.querySelector("#startGame");
+const difficulty = document.querySelector("#difficulty");
+const footer = document.querySelector(".footer");
+
 let bombs = [];
-let points = 0;
 
-//===startFunzioni==================================================================
-//Genera Numeri casuali
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-//Crea Elementi
-function createElements(tagHtml, classe, contenuto) {
 
-    //Creiamo una cella
-    const cell = document.createElement(tagHtml);
-    cell.classList.add(classe);
-    cell.innerText = contenuto;
-    return cell;
-}
-//Crea delle bombe
-function generateBombs(min, max) {
+//===ElementEnd======================================================
+
+//===EventListStart======================================================
+btnStartGame.addEventListener("click", function() {
+        //Prendo il main della pagina
+        const main = document.querySelector(".main");
+        main.innerHTML = "";
+        //prendo la difficolta del Game
+        let difficulty = Number(document.querySelector("#difficulty").value);
+        
+        let gameOver = false;
+        let point = 0;
+
+        
+        let bombe = generateBombs(16, difficulty);
+        console.log(bombe);
+        
+        
+        //Creo una DIV griglia, e gli aggiungo la classe
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
     
-    while(bombs.length !== 16) {
-        const bomb = getRandomNumber(min, max);
-        if (!bombs.includes(bomb)) {
-            bombs.push(bomb)
-        }
-    }
-}
+        //Creiamo n celle
 
-function clicked(box) {
+        let nCellTot = difficulty;
+        let misura = `calc(100% / ${Math.ceil(Math.sqrt(nCellTot))})`; //radice quadrata di nCellTot
     
-    let numberSquere = Number(box.innerText);
-    
-    if (bombs.includes(numberSquere)) {
-        let test = document.querySelectorAll(".bomb");
-        test.forEach(box => {
-            box.style.backgroundColor = 'red';
-        })
-        
-        square.removeEventListener("click", function() {
-            
-        });
-        
-        console.log("Hai perso totalizzando " + points + " punti");
-    } else {
-        points ++;
-        console.log(points);
-    }
+        for (let i = 0; i < nCellTot; i++) {
+            let cella = createObj("div", "cell", i+1);
+            cella.style.width = misura;
+            cella.style.heigt = misura;
+            if (bombe.includes(i+1)) {
+                cella.classList.add("bomb")
+            }
+            //intanto che creo le celle gli aggiun un EventListner
+            cella.addEventListener("click", function(){
+                
+                let currentCell = Number(this.innerText)
+                
+                
+                if (gameOver == false) {
+                    if (bombe.includes(currentCell)) {
+                        this.classList.add("clicked-bomb")
+                        console.log("bomba");
+                        gameOver = true;
+                        let allBomb = document.querySelectorAll(".bomb")
+                        allBomb.forEach(cella => {
+                            cella.style.backgroundColor = "red";
+                        })
+                        main.innerHTML += `hai totalizzato: ${ point } point <br> GAMEOVER`
 
-    box.classList.replace("unclicked","clicked");
-    
-    if (points == difficulty - 16) {
-        let test = document.querySelectorAll(".unclicked");
-        test.forEach(box => {
-            box.style.backgroundColor = 'red';
-            
-        })
-    }
-}
-
-//===endFunzioni==================================================================
-
-//===startEvent==================================================================
-
-btnCreateTable.addEventListener("click", function(){
-    container.innerHTML = "";
-    difficulty = Number(levelDifficuly.value);
-    
-    generateBombs(1, difficulty);
-    console.log(bombs);
-
-    for (let i = 1; i <= difficulty ; i++) {
+                    } else if (! this.classList.contains("clicked")) {
+                        this.classList.add("clicked")
+                            point++
+                            console.log(point);
+                            let score = document.getElementById("score");
+                            score.innerHTML = point;
+                            
+                    }                
+                }
+            })
         
-        let square = createElements("div", "square", i);
-        
-        square.classList.add("unclicked");
-        
-        if (difficulty == 49) {
-            square.classList.add("easySquere");
+            grid.append(cella);
         }
-        
-        if (difficulty == 81) {
-            square.classList.add("mediumSquere");
-        }
-        
-        if (difficulty == 100) {
-            square.classList.add("hardSquere");
-        }
-
-        if (bombs.includes(i)) {
-            square.classList.add("bomb")
-        }
-
-        container.append(square);
-
-        square.addEventListener("click", function(){
-            clicked(square);
-            // console.log(i);
-        })
-        
-    }
+        //Appendo la griglia al main
+        main.appendChild(grid);
 })
-
-
+//===EventListEnd======================================================
